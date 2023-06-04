25509de45303f6bf3a40dda9a2ace1819dc52bdf
MD. Amimul Ahasun Anas <anasamimulahasun@gmail.com>
2023-06-04 22:25:56 +0600

    Python If Else Condition Practice

---
+++ Python If Else7.py
@@ -0,0 +1,9 @@
+#This example misses indentations to be correct.
+
+#Insert the missing indentation to make the code correct:
+
+#if 5 > 2:
+#print("Five is greater than two!")
+
+if 5 > 2:
+  print("Five is greater than two!")
